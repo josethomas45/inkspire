bb9b38cf2c1c849fb9b2c3177ca78dc74142e9a1
Jose thomas <josethomas2026@cs.sjcetpalai.ac.in>
2025-04-26 18:34:20 +0530

    some touchup

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
+
 function Hero() {
-    return (
-        <section id="hero" className="hero">
-        <div className="hero-content">
-          <h1>WELCOME TO INKSPIRE</h1>
-          <p>Your one-stop shop for the best artistic products!</p><br></br>
-          <a href="#Featured" className="cta-btn">Shop Now</a><br></br>
-        </div>
-      </section>
-    )
-  }
-  
-  export default Hero
+  return (
+    <section id="hero" className="hero">
+      <div className="hero-content">
+        <h1>WELCOME TO INKSPIRE</h1>
+        <p>Your one-stop shop for the best artistic products!</p><br></br>
+        <a href="#Featured" className="cta-btn">Shop Now</a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
